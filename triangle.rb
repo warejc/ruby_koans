7767c49d9d3ae7d40dc021f8f269ec981502bdb4
xxx <xxx>
2019-09-09 14:29:12 -0400

    Learn about exceptions, finish triangle project

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  @ab = a + b
+  @ac = a + c
+  @cb = c + b
+  
+  if @ab <= c
+    raise TriangleError
+  elsif @ac <= b
+    raise TriangleError
+  elsif @cb <= a
+    raise TriangleError
+  end 
+  
+  if a <= 0 or b <= 0 or c <= 0
+    raise TriangleError
+  end
+
   return_val = nil
   return_val = if a == b and a == c
                 :equilateral
